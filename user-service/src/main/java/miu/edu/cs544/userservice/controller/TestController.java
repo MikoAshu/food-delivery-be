8336a957e75fce79804f9401cff48a0c6efa5e3a
Michael Ashenafi <ashenafimichael@gmail.com>
2022-09-25 11:25:23 -0500

    Addede nginx forwarding for user-service

--- user-service/src/main/java/miu/edu/cs544/userservice/controller/TestController.java
+++ user-service/src/main/java/miu/edu/cs544/userservice/controller/TestController.java
@@ -1,2 +1,13 @@
-package miu.edu.cs544.userservice.controller;public class TestController {
+package miu.edu.cs544.userservice.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String getTest(){
+        return "Hello";
+    }
 }
